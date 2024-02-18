a8b22fdfc98338d7d43e8c16f77b1545a78c2433
Jordan <j.jordan.quispe.supo@gmail.com>
2024-02-17 21:10:51 -0500

    primer commit

---
+++ repasoBasico/src/components/MiComponente.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "MiComponente",
+  data(){
+    return{
+      subtitulo: "Soy el subtitulo de mi componente.",
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Este es mi componente</h1>
+    <h2>{{subtitulo}}</h2>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
